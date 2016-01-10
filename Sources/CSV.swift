3fef6289992b87d5bab9ddf7cc6c518d6a043520
anders617 <anders617@gmail.com>
2016-01-09 21:38:14 -0500

    Made CSV public

--- Sources/CSV.swift
+++ Sources/CSV.swift
@@ -9,18 +9,18 @@
 import Foundation
 import ABMatrices
 
-class CSV {
-    private(set) var content:ABMatrix<CSVCell>
+public class CSV {
+    internal(set) var content:ABMatrix<CSVCell>
     
-    init() {
+    public init() {
         content = ABMatrix(rowCount: 1, columnCount: 1, withValue: .Empty)
     }
     
-    init(rowCount: Int, columnCount: Int, withValue value:CSVCell = .Empty) {
+    public init(rowCount: Int, columnCount: Int, withValue value:CSVCell = .Empty) {
         content = ABMatrix(rowCount: rowCount, columnCount: columnCount, withValue: value)
     }
     
-    init(withHeaders headers: ABVector<CSVCell>) {
+    public init(withHeaders headers: ABVector<CSVCell>) {
         content = ABMatrix(rowCount: 1, columnCount: headers.count, withValue: .Empty)
         for columnNum in 0..<headers.count {content[0,columnNum] = headers[columnNum].header}
     }
